8161e939b8918586c834e6babc4e3d5f86fc9b65
Yung-Jen <frank5d2008@gmail.com>
2022-05-29 21:51:21 -0400

    update some features

--- target/commodity/js/getParameter.js
+++
@@ -1,6 +0,0 @@
-// get parameter's value
-function getParameter(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i");
-    var r = location.search.substr(1).match(reg);
-    if (r!=null) return (r[2]); return null;
-}
